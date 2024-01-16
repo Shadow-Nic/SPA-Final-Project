51f3ecd8c1a540f274a4911749966ab17ac6b7ce
yezustheoneandonly <wmountain.entertainment@googlemail.com>
2024-01-16 22:44:15 +0100

    edit Header.jsx change to rsuite package

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,61 +1,25 @@
 import React, { useState } from 'react';
-
+import { Dropdown, Button } from 'rsuite';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import '../Style/Main.css';
 
-const Dropdown = ({ items }) => {
-    const [visible, setVisible] = useState(false);
-   
-    const select = (item) => {
-       
-    };
-   
-    const show = () => {
-       setVisible(true);
-       document.addEventListener("click", hide);
-    };
-   
-    const hide = () => {
-       setVisible(false);
-       document.removeEventListener("click", hide);
-    };
-   
-    const renderListItems = () => {
-       return items.map((item, index) => (
-           <Link key={index} to={item.path} onClick={() => select(item)}>
-               {item.name}
-           </Link>
-       ));
-    };
-   
+
+const Header = () => {
     return (
-       <div className={"dropdown-container" + (visible ? " show" : "")}>
-           <div className={"dropdown-display" + (visible ? " clicked": "")} onClick={show}>
-               <FontAwesomeIcon icon={faBars} style={{color: "#FFD43B",}} />
-           </div>
-           <div className="dropdown-list">
-               {renderListItems()}
-           </div>
+     <header>
+       <div className="header">
+         <img src="/src/assets/Logo-NoBG.png" alt="Fitify Logo" />
+         <div className="menue">
+           <Dropdown title={<FontAwesomeIcon icon={faBars} />}>
+             <Dropdown.Item as={Link} to="/profile">Profile</Dropdown.Item>
+             <Dropdown.Item as={Link} to="/eat">Eat</Dropdown.Item>
+             <Dropdown.Item as={Link} to="/sports">Sports</Dropdown.Item>
+           </Dropdown>
+         </div>
        </div>
-    );
-   };
-   
-   const Header = () => {
-    return (
-       <header>
-           <div className="header">
-               <img src="/src/assets/Logo-NoBG.png" alt="Fitify Logo" />
-               <div className="menue">
-                 <Dropdown items={[
-                    { name: 'Profile', path: '/' },
-                    { name: 'Eat', path: '/' },
-                    { name: 'Sports', path: '/' }
-                 ]} />
-               </div>
-           </div>
-       </header>
+     </header>
     );
    };
    
