a4674d85c8ac08ad028104ff17ec0125644a1844
Shadow-Nic <nicbasler@gmail.com>
2024-01-17 13:47:19 +0100

    merge fix

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
 
 
-const DoughnutChart = ({ labels, data, backgroundColor, borderColor }) => {
+const DoughnutChart = ({ title, labels, data, backgroundColor, borderColor }) => {
 
     const chartRef = React.useRef(null);
     const chartData = {
