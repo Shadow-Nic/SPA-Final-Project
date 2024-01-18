39854b3fc6f1112297afc581594e918a100515c0
Daniel Wolthausen <daniel.wolthausen@t-online.de>
2024-01-18 02:49:46 +0100

    tests

--- src/components/addDay.jsx
+++ src/components/addDay.jsx
@@ -3,144 +3,74 @@ import { useNavigate } from 'react-router-dom';
 import { BmiContext } from '../context/BmiContext';
 import { Popover, Whisper } from 'rsuite';
 import '../Style/addDay.css'
-
-
+import useSWR from 'swr';
 
 function addDay() {
-    const { dispatch, state, selectedProfile } = useContext(BmiContext);
-    const navigate = useNavigate();
+ const { dispatch, state, selectedProfile } = useContext(BmiContext);
+ const [searchTerm, setSearchTerm] = React.useState('');
 
-    const selectedProfileIndex = state.findIndex(profile => profile === selectedProfile);
+ const date = new Date();
+ const currentDate = date.toLocaleDateString('en-GB', {
+ day: '2-digit',
+ month: '2-digit',
+ year: '2-digit'
+ }).replace(/(\d{2})\/(\d{2})\/(\d{2})/, "$1.$2.$3"); //12.12.12 > 12 december 2012
 
+ const apiKey = 'hWdaj14WL8WDgVYei8imulkY2hKH8uxTfiDUvVHP';
 
-    const date = new Date();
-    const currentDate = date.toLocaleDateString('en-GB', {
-        day: '2-digit',
-        month: '2-digit',
-        year: '2-digit'
-    }).replace(/(\d{2})\/(\d{2})\/(\d{2})/, "$1.$2.$3"); //12.12.12 > 12 december 2012
+ const fetcher = async (url) => {
+ const res = await fetch(url, {
+   method: 'GET',
+   headers: { 'X-Api-Key': apiKey },
+   contentType: 'application/json',
+ });
+ if (!res.ok) throw new Error(res.status);
+ return res.json();
+ };
 
-    const addCalories = () => {
-        dispatch({
-            type: 'ADD_DAY',
-            payload: {
-                profileIndex: selectedProfileIndex,
-                dayData: {
-                    date: currentDate,
-                    todayCalories,
-                    todayProteins,
-                    todayFats,
-                    todayCarbs
-                }
-            }
-        });
-        navigate('/');
-    };
+ const FoodList = () => {
+ const { data, error } = useSWR(`https://api.api-ninjas.com/v1/nutrition?query=${searchTerm}`, fetcher);
 
-    // Fetch
+ if (error){console.log(error)
+  return <div>Failed to load</div>} ;
+ if (!data) return <div>Loading...</div>;
+ 
+ const food = data.map((item) => ({
+   name: item.name,
+   calories: item.calories,
+   servingSizeG: item.serving_size_g,
+   fatTotalG: item.fat_total_g,
+   // Add other properties as needed
+ }));
 
-    const apiKey = 'hWdaj14WL8WDgVYei8imulkY2hKH8uxTfiDUvVHP';
-    const [query, setQuery] = useState('');
-    const [nutritionFacts, setNutritionFacts] = useState([]);
-    const [todayCalories, setTodayCalories] = useState(0);
-    const [todayFats, setTodayFats] = useState(0);
-    const [todayProteins, setTodayProteins] = useState(0);
-    const [todayCarbs, setTodayCarbs] = useState(0);
-    const [food, setfood] = useState([])
+ return (
+  <div>
+    {food.map((item, index) => (
+      <div key={index}>
+        <p>{item.name}</p>
+        <p>{item.calories} calories</p>
+        <p>{item.servingSizeG}g serving size</p>
+        <p>{item.fatTotalG}g total fat</p>
+        {/* Display other properties as needed */}
+      </div>
+    ))}
+  </div>
+ );
+ };
 
+ return (
+ <div className='addDay'>
+ <input
+ type="text"
+ placeholder="Search for food..."
+ value={searchTerm}
+ onChange={e => setSearchTerm(e.target.value)}
+/>
+               <button >add Calories to Profile</button>
+               <FoodList />
+ </div>
 
-    const handleSubmit = async (query) => {
-        // Api anfrage um die Nährstoffwerte zu bekommen
-        const url = `https://api.api-ninjas.com/v1/nutrition?query=${query}`;
-
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: { 'X-Api-Key': apiKey },
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        setNutritionFacts(data);
-    };
-
-
-    const handleWeight = (index, weight) => {
-        // berechnet die Nährstoffe der Menge die der User zu sich genommen hat
-        const food = nutritionFacts[index];
-        const weightInput = window.prompt(`Please enter the weight of ${food.name} in grams:`);
-        if (weightInput) {
-            const weight = parseFloat(weightInput);
-
-            const {
-                carbohydrates_total_g,
-                fat_total_g,
-                protein_g,
-                calories,
-                serving_size_g,
-                name
-            } = food;
-
-            addNutrients(
-                calories * (weight / serving_size_g),
-                fat_total_g * (weight / serving_size_g),
-                protein_g * (weight / serving_size_g),
-                carbohydrates_total_g * (weight / serving_size_g)
-            );
-
-            addFood(name, weight);
-        }
-    };
-
-    function addNutrients(calories, fats, proteins, carbs) {
-        setTodayCalories((prevCalories) => prevCalories + calories);
-        setTodayFats((prevFats) => prevFats + fats);
-        setTodayProteins((prevProteins) => prevProteins + proteins);
-        setTodayCarbs((prevCarbs) => prevCarbs + carbs);
-    }
-    function addFood(name, weight) {
-        setfood((prevFood) => [...prevFood, { name, weight }]);
-    }
-
-
-    return (
-        <div className='addDay'>
-            <div className='display'>
-                <div className='con1'>
-
-                </div>
-                <div className='con2'>
-                    <p>Today's Calories: {todayCalories}</p>
-                    <ul>
-                        {food.map((food, index) => (
-                            <li key={index}>
-                                {food.weight}g {food.name}
-                            </li>
-                        ))}
-                    </ul>
-                    <button onClick={addCalories}>add Calories to Profile</button>
-                </div>
-            </div>
-            <div>
-                <form onSubmit={(e) => { e.preventDefault(); handleSubmit(query); }}>
-                    <input type="text" onChange={(e) => setQuery(e.target.value)} />
-                    <button type="submit">Search</button>
-                </form>
-                <div>
-                    <p>Result:</p>
-                    {nutritionFacts.map((fact, index) => (
-                        <p key={index} onClick={() => handleWeight(index)} className='resultItem'>
-                            {fact.name}
-                        </p>
-                    ))}
-                </div>
-            </div>
-        </div>
-
-    );
+ );
 }
 
 export default addDay;
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'https://api.api-ninjas.com/v1'
+    }
+  },
   plugins: [react()],
+  
 })
