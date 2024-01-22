0826af2becd480c5c15f1229727d256b4e7f2d3e
Shadow-Nic <nicbasler@gmail.com>
2024-01-22 09:07:51 +0100

    last chart edit

--- src/components/weekChart.jsx
+++ src/components/weekChart.jsx
@@ -1,43 +1,40 @@
-
-import React, { useContext, useMemo } from 'react';
-import { calculateCalories } from './getChartData';
+import React, { useContext } from 'react';
 import { BmiContext } from '../context/BmiContext';
 import { BarChart, YAxis, Bars, Line } from '@rsuite/charts';
+import { calculateCalories } from './getChartData';
 
 const WeekChart = () => {
     const { selectedProfile } = useContext(BmiContext);
 
-    const last7DaysData = selectedProfile.days.slice(0, 7).map(day => {
+    const lastSevenDaysData = selectedProfile.days.slice(0, 7).map((day) => {
         const { goalCalories, achievedCalories, bmr } = calculateCalories(day.foods, day.sports);
         return { ...day, goalCalories, achievedCalories };
     });
 
+    const dailyData = lastSevenDaysData.map((item) => [
+        item.date,
+        parseInt(item.achievedCalories, 10),
+        parseInt(item.goalCalories, 10),
+    ]);
 
-    const data = last7DaysData.map(item => [item.date, parseInt(item.achievedCalories), parseInt(item.goalCalories)]);
+    const minAchievedCalories = Math.min(...lastSevenDaysData.map((item) => parseInt(item.achievedCalories, 10))) - 100;
 
-    // Calculate the minimum value for YAxis
-    const minAchievedCalories = useMemo(() => {
-        return Math.min(...last7DaysData.map(item => parseInt(item.achievedCalories))) - 100;
-    }, [last7DaysData]);
-
-    const style = {
+    const chartStyle = {
         height: 250,
-        width: 330
-    }
-    const config = {
+        width: 330,
+    };
+
+    const chartConfig = {
         legend: {
             top: '0',
         },
-        "color": [
-            "#34c3ff",
-            "#F8C88E",
-        ],
+        color: ['#34c3ff', '#F8C88E'],
     };
 
     return (
-        <div className='Wrapper' >
+        <div className="Wrapper">
             <h3>Weekly</h3>
-            <BarChart data={data} style={style} option={config} >
+            <BarChart data={dailyData} style={chartStyle} option={chartConfig}>
                 <YAxis minInterval={200} min={minAchievedCalories - 500} axisLabel={(value) => `${value / 1}`} />
                 <Bars name="Kcal" />
                 <Line name="Required" />
